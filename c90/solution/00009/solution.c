fa97109982a37f440ade46df97d3fe1a9c866149
wrowley <william.rowley@gmail.com>
2014-08-16 16:04:23 +1000

    Adding Problem 9's name/description.

--- c90/solution/00009/solution.c
+++ c90/solution/00009/solution.c
@@ -1,5 +1,11 @@
 /**
-  * Problem description goes here
+  * A Pythagorean triplet is a set of three natural numbers, a < b < c, for which,
+  *
+  * a^2 + b^2 = c^2
+  * For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
+  *
+  * There exists exactly one Pythagorean triplet for which a + b + c = 1000.
+  * Find the product abc.
   */
 
 #include <stdio.h>
@@ -71,7 +77,7 @@ render
 
 static const euler_solution problem00009 =
 {
-    /* name   */ "Problem Name",
+    /* name   */ "Special Pythagorean triplet",
     /* memory */ &memory,
     /* solve  */ &solve,
     /* render */ &render,
