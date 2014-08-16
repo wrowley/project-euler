dbf0ac311d6dc17a3671372b95a5987ae91fbbd8
wrowley <william.rowley@gmail.com>
2014-08-16 15:07:35 +1000

    Updating docs in eulersolution.h

--- c90/common/eulersolution.h
+++ c90/common/eulersolution.h
@@ -5,8 +5,7 @@
 
 #include <stddef.h>
 
-/* This is a convenience typedef, as solutions might like to define a struct
- * to help them organise their data */
+/* Each solution must implement this struct privately. */
 typedef struct euler_state_s euler_state;
 
 /* This struct enables solutions to expose only the necessary details required
@@ -18,19 +17,21 @@ typedef struct euler_solution_s
     const char            *name;
 
     /* This function must return the amount of memory required by the solution's
-     * solve and print functions */
+     * solve function. This can be thought of as the sum of size of a solution's
+     * euler_state_s and its scratch requirements. */
     size_t                (*memory)();
 
     /* This function must solve the problem in its entirety. It takes a single
      * argument of a buffer of a size returned by memory(). It must be able
      * to solve the problem without any assumptions about the contents of
-     * the buffer. */
+     * the buffer. It returns a pointer to the solution's implementation
+     * of euler_state. */
     euler_state*          (*solve)(void *);
 
     /* This function simply exists to render the solution of the problem to
      * a buffer of chars. Behaviour is undefined unless its first argument is
-     * a pointer to the same address as provided in the most recent call to solve().
-     * This function expects the contents of the buffer not to have changed since
+     * a pointer to the same pointer as returned by the most recent call to solve().
+     * This function expects the contents of its state not to have changed since
      * solve() was last called. The second argument must be a pointer to a buffer
      * of chars guaranteed to be large enough to fit the whole solution, just as
      * it would be copy-pasted into the project-euler website for any
